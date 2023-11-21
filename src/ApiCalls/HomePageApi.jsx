dc541dfdfba299e5c45b6b63f49e0d681a611279
vishalpanchal27 <112761001+vishalpanchal27@users.noreply.github.com>
2023-11-22 02:07:45 +0530

    Update HomePageApi.jsx

--- src/ApiCalls/HomePageApi.jsx
+++ src/ApiCalls/HomePageApi.jsx
@@ -24,7 +24,7 @@ const HomePageApi = () => {
                 sort_by: 'relevance'
             },
             headers: {
-                'X-RapidAPI-Key': 'fc3539ca20msh36a16b5c3e8e1b3p1640b7jsn60b346470444',
+                'X-RapidAPI-Key': 'ceaa6afcaamsh99619baf83e6f76p14868cjsn80d47a7979f4',
                 'X-RapidAPI-Host': 'target1.p.rapidapi.com'
             }
         };
