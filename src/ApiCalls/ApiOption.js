4db425eea0cbe1990e6d5097883413db8d3d8394
vishalpanchal27 <112761001+vishalpanchal27@users.noreply.github.com>
2023-11-24 01:35:32 +0530

    Update ApiOption.js

--- src/ApiCalls/ApiOption.js
+++ src/ApiCalls/ApiOption.js
@@ -5,7 +5,7 @@ export const categoryApi = {
     url: 'https://target1.p.rapidapi.com/categories/v2/list',
     params: { category: '5xtg6' },
     headers: {
-        'X-RapidAPI-Key': 'fc3539ca20msh36a16b5c3e8e1b3p1640b7jsn60b346470444',
+        'X-RapidAPI-Key': 'ceaa6afcaamsh99619baf83e6f76p14868cjsn80d47a7979f4',
         'X-RapidAPI-Host': 'target1.p.rapidapi.com'
     }
 };
