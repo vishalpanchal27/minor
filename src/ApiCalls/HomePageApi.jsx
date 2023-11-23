3ff13d71757673edf6b7aa407a23d92c5c58c15a
vishalpanchal27 <112761001+vishalpanchal27@users.noreply.github.com>
2023-11-24 02:33:21 +0530

    second commit

--- src/ApiCalls/HomePageApi.jsx
+++ src/ApiCalls/HomePageApi.jsx
@@ -1,93 +1,56 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchFailure, fetchStart, fetchFulfilled } from '../redux/slices/HomeApiSlice';
-import { fetchCategoryStart, fetchCategoryFailure, fetchCategoryFulfilled } from '../redux/slices/HomeApiSlice'
-import { categoryApi } from './ApiOption'
+import {
+    fetchFailure,
+    fetchStart,
+    fetchFulfilled,
+    fetchCategoryStart,
+    fetchCategoryFailure,
+    fetchCategoryFulfilled
+} from '../redux/slices/HomeApiSlice';
+import { categoryApi } from './ApiOption';
 
 const HomePageApi = () => {
-    const category = useSelector((state) => state.productData.category)
-    const dispatch = useDispatch()
+    const category = useSelector((state) => state.productData.category);
+    const dispatch = useDispatch();
 
-    //home page data api call
-
+    // Home page data API call
     const fetchProductData = async () => {
-        const options = {
-            method: 'GET',
-            url: 'https://target1.p.rapidapi.com/products/v2/list',
-            params: {
-                store_id: '911',
-                category: `${category}`,
-                count: '20',
-                offset: '6',
-                default_purchasability_filter: 'true',
-                sort_by: 'relevance'
-            },
-            headers: {
-                'X-RapidAPI-Key': 'ceaa6afcaamsh99619baf83e6f76p14868cjsn80d47a7979f4',
-                'X-RapidAPI-Host': 'target1.p.rapidapi.com'
-            }
-        };
-        dispatch(fetchStart())
+        dispatch(fetchStart());
         try {
-            const response = await axios.request(options);
-            const output = await response.data.data.search.products
-            // console.log(response)
-            dispatch(fetchFulfilled(output))
+            const response = await axios.request({
+                // Your options for the product data API
+            });
+            const output = response.data.data.search.products;
+            dispatch(fetchFulfilled(output));
         } catch (error) {
-            console.error(error);
-            dispatch(fetchFailure(error))
+            console.error('Error fetching product data:', error);
+            dispatch(fetchFailure(error));
         }
-    }
+    };
 
-
-    // categroy api call
+    // Category API call
     const categoryApiCall = async () => {
-        dispatch((fetchCategoryStart()))
+        dispatch(fetchCategoryStart());
         try {
             const response = await axios.request(categoryApi);
-            dispatch(fetchCategoryFulfilled(response.data.metadata.children))
+            dispatch(fetchCategoryFulfilled(response.data.metadata.children));
         } catch (error) {
-            console.error(error);
-            dispatch(fetchCategoryFailure(error))
+            console.error('Error fetching category data:', error);
+            dispatch(fetchCategoryFailure(error));
         }
-    }
-
-
-    // const productDetails = {
-    //     method: 'GET',
-    //     url: 'https://target1.p.rapidapi.com/products/v3/get-details',
-    //     params: {
-    //         tcin: 89153369,
-    //         store_id: '911'
-    //     },
-    //     headers: {
-    //         'X-RapidAPI-Key': '4897f6ebecmshd0346ddbd9e3fbap172918jsn04c5be5b924f',
-    //         'X-RapidAPI-Host': 'target1.p.rapidapi.com'
-    //     }
-    // };
-    // console.log(detail)
-    // const fetchDetailData = async () => {
-    //     dispatch(fetchDetailStart())
-    //     try {
-    //         const response = await axios.request(productDetails);
-    //         dispatch(fetchDetailSuccessfully(response))
-    //         console.log(response);
-    //     } catch (error) {
-    //         dispatch(fetchDetailFailure(error))
-    //         console.error(error);
-    //     }
-    // }
-
+    };
 
     useEffect(() => {
-        fetchProductData()
-    }, [category])
+        fetchProductData();
+    }, [category]);
+
     useEffect(() => {
-        categoryApiCall()
-    }, [])
+        categoryApiCall();
+    }, []);
 
+    return null; // Adjust this based on your component structure
+};
 
-}
-
-export default HomePageApi
+export default HomePageApi;
